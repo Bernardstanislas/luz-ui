9ddd06feea9f3962bb9f85c8fcb68cb6798c7f28
Stanislas Bernard <bernardstanislas@gmail.com>
2016-06-03 12:51:14 +0200

    Change accent color

--- src/style/palette.js
+++ src/style/palette.js
@@ -3,7 +3,7 @@ export default {
     primaryColor:        '#3F51B5',
     primaryColorLight:  '#C5CAE9',
     primaryColorText:   '#FFFFFF',
-    accentColor:         '#FF4081',
+    accentColor:         '#009688',
     primaryTextColor:   '#212121',
     secondaryTextColor: '#727272',
     dividerColor:        '#B6B6B6'
